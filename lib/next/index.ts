bb9e01749b7746d7d864c82915261902c2726423
Zioth <louisgiet.w@gmail.com>
2023-10-24 22:57:18 +0200

    Renamed link to component style Link

--- lib/next/index.ts
+++ lib/next/index.ts
@@ -3,4 +3,4 @@ export { createUrl } from './utils'
 export * as routers from './routers'
 export * from './routers'
 
-export { default as Link } from './link'
+export { default as Link } from './Link'
