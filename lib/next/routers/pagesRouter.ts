37bc243559a9d72ff518eed24a9903d925000a00
Zioth <louisgiet.w@gmail.com>
2023-11-16 17:04:45 +0100

    fix: funky pagesRouter types

--- lib/next/routers/pagesRouter.ts
+++ lib/next/routers/pagesRouter.ts
@@ -3,9 +3,11 @@ import { useRouter as usePagesRouter } from 'next/router'
 import { type Route, createUrl } from '../utils'
 import type { Routes } from '../../types/routes'
 
-type RouteData<Path extends Routes.Paths.All> = Route.Data<Path> & {
-  path: Path
-}
+type RouteData<Path extends Routes.Paths.All> =
+  & {
+    path: Path
+  }
+  & (Route.Data<Path> extends never ? {} : Route.Data<Path>)
 
 export function useRouter<T extends Routes.Paths.PagesDir>(currentPath?: T) {
   const { push, replace, prefetch, query, ...router } = usePagesRouter()
@@ -16,12 +18,13 @@ export function useRouter<T extends Routes.Paths.PagesDir>(currentPath?: T) {
       /** Callback to execute before replacing router state */
       beforePopState: (cb: (state: NextHistoryState) => boolean) => void,
     }),
+    /** Contains both the route path params and the url query parameters */
     query: query as
       & Routes.Params.get<
         // @ts-ignore
         Routes.getByPath<Routes.PagesDir, T>
       >
-      & Record<string, string>,
+      & Record<string, string | string[]>,
     push<
       Path extends Routes.Paths.All,
       As extends Routes.Paths.All
@@ -31,9 +34,8 @@ export function useRouter<T extends Routes.Paths.PagesDir>(currentPath?: T) {
       options?: TransitionOptions
     ): Promise<boolean> {
       return push(
-        // 'path' in url ? createUrl(url.path, url) : url,
-        createUrl(url.path, url),
-        as === undefined ? undefined : createUrl(as.path, as),
+        createUrl(url.path, url as any),
+        as === undefined ? undefined : createUrl(as.path, as as any),
         options
       );
     },
@@ -48,12 +50,12 @@ export function useRouter<T extends Routes.Paths.PagesDir>(currentPath?: T) {
     ): Promise<boolean> {
       return replace(
         // 'path' in url ? createUrl(url.path, url) : url,
-        createUrl(url.path, url),
-        as === undefined ? undefined : createUrl(as.path, as),
+        createUrl(url.path, url as any),
+        as === undefined ? undefined : createUrl(as.path, as as any),
         options
       );
     },
-    
+
     prefetch<
       Path extends Routes.Paths.All,
       As extends Routes.Paths.All
@@ -63,8 +65,8 @@ export function useRouter<T extends Routes.Paths.PagesDir>(currentPath?: T) {
       options?: Parameters<typeof prefetch>[2]
     ): Promise<void> {
       return prefetch(
-        createUrl(url.path, url),
-        as === undefined ? undefined : createUrl(as.path, as),
+        createUrl(url.path, url as any),
+        as === undefined ? undefined : createUrl(as.path, as as any),
         options,
       );
     },
@@ -73,13 +75,13 @@ export function useRouter<T extends Routes.Paths.PagesDir>(currentPath?: T) {
 
 // Next types that are private and are needed to be redeclared...
 interface TransitionOptions {
-    shallow?: boolean;
-    locale?: string | false;
-    scroll?: boolean;
-    unstable_skipClientCache?: boolean;
+  shallow?: boolean;
+  locale?: string | false;
+  scroll?: boolean;
+  unstable_skipClientCache?: boolean;
 }
 interface NextHistoryState {
-    url: string;
-    as: string;
-    options: TransitionOptions;
+  url: string;
+  as: string;
+  options: TransitionOptions;
 }
